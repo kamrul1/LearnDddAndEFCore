eda3de86eb881d82a1f2545a8192e1d9943c72fa
kamrul.islam <kamrul.islam@dotnet-specialist.co.uk>
2021-11-04 18:41:40 +0000

    Add project files.

---
+++ App/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = GetConnectionString();
+            ILoggerFactory loggerFatory = CreateLoggerFactory();
+
+
+            var optionsBuilder = new DbContextOptionsBuilder<SchoolContext>();
+            optionsBuilder
+                .UseSqlServer(connectionString)
+                .UseLoggerFactory(loggerFatory)
+                .EnableSensitiveDataLogging();
+
+            using (var context = new SchoolContext(optionsBuilder.Options))
+            {
+                Student student = context.Students.Find(1L);
+
+                student.Name += "2";
+                student.Email += "2";
+
+                context.SaveChanges();
+            }
+        }
+
+        private static ILoggerFactory CreateLoggerFactory()
+        {
+            return LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter((category, level) =>
+                        category == DbLoggerCategory.Database.Command.Name && level == LogLevel.Information)
+                    .AddConsole();
+
+            });
+        }
+
+        private static string GetConnectionString()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            return configuration["ConnectionString"];
+
+        }
+    }
+}
---
+++ build/README.md
@@ -0,0 +1,9 @@
+﻿# Learn DDD and EF Core: Preserving Encapsulation
+
+This are my course notes follownig along the course on [Pluralsight](https://app.pluralsight.com/library/courses/ddd-ef-core-preserving-encapsulation/table-of-contents).
+
+
+---
+
+
+
